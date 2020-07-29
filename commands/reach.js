4f0b3f52ba5f0b79dedced4af398ce2389828d4b
zholly15 <zholly15@gmail.com>
2020-07-29 14:03:53 -0700

    Initial Commit

---
+++ commands/reach.js
@@ -0,0 +1,8 @@
+module.exports = {
+	name: 'reach',
+	description: 'Slipspace Contact, Designation Reach. CLASSIFED SECTION A1',
+	cooldown: 5,
+	execute(message) {
+		message.channel.send('https://www.youtube.com/watch?v=Q_4i-yOUmXY');
+	},
+};
